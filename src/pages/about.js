d859d6fdf26faad6e7a5d0e52de7c13b72a1b5c1
sundongzhi <sundongzhi@gozap.com>
2021-01-22 11:24:21 +0800

    :bug: fix(gatsby):
    Signed-off-by: sundongzhi <sundongzhi@gozap.com>

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function About() {
+  return (
+    <Layout>
+      <div style={{ color: `teal` }}>
+        <p>
+          I’m good enough, I’m smart enough, and gosh darn it, people like me!
+        </p>
+      </div>
+    </Layout>
+  )
+}
